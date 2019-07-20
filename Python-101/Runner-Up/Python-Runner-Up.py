514a72aa8f3ef1f6fc8ebad5071afe18b4431c18
alali@abdullah-alali.com <31869379+aza0092@users.noreply.github.com>
2019-07-20 09:32:59 -0500

    Runner-Up

---
+++ Python-101/Runner-Up/Python-Runner-Up.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr2 = arr[:n]
+    arr2.sort()
+    arr2 = list(dict.fromkeys(arr2)) #remove duplicates, dict can have duplicate keys
+    print(arr2[len(arr2)-2])
