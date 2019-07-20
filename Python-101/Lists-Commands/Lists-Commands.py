e25003e364f9337174dd8244c294da87f3202fb4
alali@abdullah-alali.com <31869379+aza0092@users.noreply.github.com>
2019-07-20 11:00:35 -0500

    Update Lists-Commands.py

--- Python-101/Lists-Commands/Lists-Commands.py
+++ Python-101/Lists-Commands/Lists-Commands.py
@@ -2,18 +2,19 @@ if __name__ == '__main__':
   N = int(input())
   output_list = []
   for i in range(N):
-    command = input().split()
-    if command[0] == 'insert':
-      output_list.insert(int(command[1]), int(command[2]))
-    if command[0] == 'print':
+    read_inp = input().split()
+    command = read_inp[0]
+    if command == 'insert':
+      output_list.insert(int(read_inp[1]), int(read_inp[2]))
+    if command == 'print':
       print(output_list)
-    if command[0] == 'remove':
-      output_list.remove(int(command[1]))
-    if command[0] == 'append':
-      output_list.append(int(command[1]))
-    if command[0] == 'sort':
+    if command == 'remove':
+      output_list.remove(int(read_inp[1]))
+    if command == 'append':
+      output_list.append(int(read_inp[1]))
+    if command == 'sort':
       output_list.sort()
-    if command[0] == 'pop':
+    if command == 'pop':
       output_list.pop()
-    if command[0] == 'reverse':
+    if command == 'reverse':
       output_list.reverse()
